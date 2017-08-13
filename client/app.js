24ad93cf0ee8dd434fa6c521f7fbc660ff51f93a
Thomas Beirne <thomas@buildingconnected.com>
2017-08-13 15:09:23 -0700

    Add boilerplate for style/css-loaders and axios for XHR

--- client/app.js
+++ client/app.js
@@ -1,1 +1,15 @@
-console.log('Insert awesome solution here')
+import React, { Component } from 'react'
+
+import './styles.css'
+
+class App extends Component {
+	render() {
+		return (
+			<div className="app-placeholder">
+				INSERT AWESOME FILES CHALLENGE SOLUTION HERE
+			</div>
+		)
+	}
+}
+
+export default App
--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,24 @@
 'use strict'
 
 module.exports = {
-	entry: { app: './client/app.js' },
+	entry: { app: './client/index.js' },
 	output: {
-		filename: 'app.js',
+		filename: 'index.js',
 		path: '/',
 		publicPath: '/dist',
 	},
 	module: {
-		rules: [{
-			test: /\.js$/,
-			exclude: /node_modules/,
-			use: 'babel-loader',
-		}],
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: 'babel-loader',
+			},
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader'],
+			},
+		],
 	},
 	performance: {
 		hints: false,
